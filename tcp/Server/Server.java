bd88f75f1a9959c650a8791e9e3bc7771813979e
Duarte <guiduart3223@gmail.com>
2024-03-27 22:40:44 -0300

    Server Side TCP

--- tcp/Server/Server.java
+++ tcp/Server/Server.java
@@ -1,18 +1,61 @@
 package tcp.Server;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
 
-        DatagramSocket server = new DatagramSocket(9090);
-        byte[] b = new byte[256];
-        DatagramPacket packet = new DatagramPacket(b, b.length);
-        server.receive(packet);
-        String response = new String(packet.getData());
-        System.out.println("Response Data: " +  response);
-        server.close();
+    @SuppressWarnings("resource")
+    public static void main(String[] args) throws IOException{
+        
+        Socket socket = null;
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        ServerSocket serverSocket = null;
+    
+        serverSocket = new ServerSocket(8080);
+    
+        while(true)
+        {
+            try {
+    
+                socket = serverSocket.accept();
+    
+                inputStreamReader = new InputStreamReader(socket.getInputStream());
+                outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+    
+                bufferedReader = new BufferedReader(inputStreamReader);
+                bufferedWriter = new BufferedWriter(outputStreamWriter);
+    
+                while (true) {
+                    String messageClient = bufferedReader.readLine();
+    
+                    System.out.println("Client: " + messageClient);
+    
+                    bufferedWriter.write("MSG Recived");
+                    bufferedWriter.newLine();
+                    bufferedWriter.flush();
+    
+                    if (messageClient.equalsIgnoreCase("exit"))
+                        break;
+                }
+    
+                socket.close();
+                inputStreamReader.close();
+                outputStreamWriter.close();
+                bufferedReader.close();
+                bufferedWriter.close();
+    
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
