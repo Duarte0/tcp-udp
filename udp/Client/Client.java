18a2a111ba8c524dc5c46bf9282893cd854586dc
Duarte <guiduart3223@gmail.com>
2024-03-27 22:41:03 -0300

    Client Side UDP

--- udp/Client/Client.java
+++ udp/Client/Client.java
@@ -1,66 +1,19 @@
-package udp.Client;
 
-import java.net.*;
-import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class Client {
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws IOException {
 
-        // conexao e comunicacao
-        Socket socket = null;
-        InputStreamReader inputStreamReader = null;
-        OutputStreamWriter outputStreamWriter = null;
-        BufferedReader bufferedReader = null;
-        BufferedWriter bufferedWriter = null;
-
-        try {
-            // estabelece conexao
-            socket = new Socket("localhost", 8080);
-
-            // conversao byte para caractere
-            inputStreamReader = new InputStreamReader((socket.getInputStream()));
-            outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
-            // leitura e escrita
-            bufferedReader = new BufferedReader(inputStreamReader);
-            bufferedWriter = new BufferedWriter(outputStreamWriter);
-
-            try (Scanner scanner = new Scanner(System.in)) {
-                //permite troca de mensagem
-                while (true) {
-                    String message = scanner.nextLine();
-                    bufferedWriter.write(message);
-                    bufferedWriter.newLine();
-                    bufferedWriter.flush();
-
-                    System.out.println("Server: " + bufferedReader.readLine());
-
-                    if (message.equalsIgnoreCase("exit"))
-                        break;
-                }
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (socket != null)
-                    socket.close();
-                if (inputStreamReader != null)
-                    inputStreamReader.close();
-                if (outputStreamWriter != null)
-                    outputStreamWriter.close();
-                if (bufferedReader != null)
-                    bufferedReader.close();
-                if (bufferedWriter != null)
-                    bufferedWriter.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        DatagramSocket socket = new DatagramSocket();
+        InetAddress address = InetAddress.getByName("localhost");
+        String string = "Hello World";
+        byte[] b = string.getBytes();
+        DatagramPacket packet = new DatagramPacket(b, b.length, address, 9090);
+        socket.send(packet);
+        socket.close();
     }
 }
