914a08c2beccf7f7afb30ab2646b6611f22f587d
Duarte <guiduart3223@gmail.com>
2024-03-27 23:04:40 -0300

    Server Side UDP

--- udp/Server/Server.java
+++ udp/Server/Server.java
@@ -3,16 +3,32 @@ package udp.Server;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.sql.Timestamp;
 
-public class Server {
+public class Server extends Thread {
     public static void main(String[] args) throws IOException {
 
-        DatagramSocket server = new DatagramSocket(9090);
-        byte[] b = new byte[256];
-        DatagramPacket packet = new DatagramPacket(b, b.length);
-        server.receive(packet);
-        String response = new String(packet.getData());
-        System.out.println("Response Data: " +  response);
-        server.close();
+        @SuppressWarnings("resource")
+        DatagramSocket socket = new DatagramSocket(9090);
+        byte[] b = new byte[1042];
+        DatagramPacket receivePacket;
+
+        System.out.println("Server on");
+
+        while (true) {
+            receivePacket = new DatagramPacket(b, b.length);
+            socket.receive(receivePacket);
+            InetAddress address = receivePacket.getAddress();
+
+            int port = receivePacket.getPort();
+
+            String clientMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
+
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+
+            System.out.println("[" + timestamp + "]" + address + port + " - " + clientMessage);
+             
+        }
     }
 }
