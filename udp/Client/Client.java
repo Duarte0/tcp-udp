919220524c1b641c0c3eaf4a88afe6d730487f4a
Duarte <guiduart3223@gmail.com>
2024-03-27 23:04:26 -0300

    Client Side UDP

--- udp/Client/Client.java
+++ udp/Client/Client.java
@@ -4,16 +4,35 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws IOException {
 
-        DatagramSocket socket = new DatagramSocket();
+         DatagramPacket packet;
         InetAddress address = InetAddress.getByName("localhost");
-        String string = "Hello World";
-        byte[] b = string.getBytes();
-        DatagramPacket packet = new DatagramPacket(b, b.length, address, 9090);
-        socket.send(packet);
-        socket.close();
+        byte[] buf;
+
+        DatagramSocket socket = new DatagramSocket();
+
+        socket.setSoTimeout(1000);
+        System.out.println("Type a message to send to the server.");
+        System.out.println("Type 'exit' to exit.");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String cmd = scanner.nextLine();
+
+            if (cmd.equals("exit")) {
+                socket.close();
+                System.exit(1);
+                scanner.close();
+            }
+
+            buf = cmd.getBytes();
+            packet = new DatagramPacket(buf, buf.length, address, 9090);
+            socket.send(packet);
+        }
     }
 }
